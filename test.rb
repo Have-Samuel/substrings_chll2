95ef8d3f106dc799fcfa97cb05b19a862f0fe49d
samuel <samiehave9@gmail.com>
2024-08-22 12:59:13 +0300

    Checking out the methods

---
+++ test.rb
@@ -0,0 +1,32 @@
+str = 'Hello World'
+
+chars = str.chars
+# returns ['H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd']
+# and for the index => 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+# ['H' => 0, 'e' => 1, 'l' => 2, 'l' => 3, 'o' => 4, ' ' => 5, 'W' => 6, 'o' => 7, 'r' => 8, 'l' => 9, 'd' => 10]
+
+puts chars[6]
+# returns 'W'
+# What does chars[6] return?
+# chars[6] returns 'W'
+# What does chars[6, 2] return?
+# chars[6, 2] returns ['W', 'o']
+# What does chars[7..10] return?
+# chars[7..10] returns ['o', 'r', 'l', 'd']
+# What does chars[7...10] return?
+# chars[7...10] returns ['o', 'r', 'l']
+# What does chars[7..-1] return?
+# chars[7..-1] returns ['o', 'r', 'l', 'd']
+# what does chars[5, str.length - 1] return?
+# chars[5, str.length - 1] returns [' ', 'W', 'o', 'r', 'l', 'd']
+# What does chars[6, str.length - 1] return?
+# chars[6, str.length - 1] returns ['W', 'o', 'r', 'l', 'd']
+# What does chars[7, str.length - 1] return?
+# chars[7, str.length - 1] returns ['W', 'o', 'r', 'l', 'd']
+
+# Waht does [str.lingth - 1] mean?
+# str.length - 1 is the index of the last character in the string, and retutns 10
+# What about word = str[7, str.length - 1]?
+# word = str[7, str.length - 1] returns 'World' because it starts at index 7 and goes to the end of the string
+# Why does str[7, str.length - 1], begin at 'w' and not 'o'?
+# str[7, str.length - 1] begins at 'W' because the first argument is the starting index, and the second argument is the length of the substring
