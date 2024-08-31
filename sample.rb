7bbcb52020ae52edf0aa06235515406ae3a33cb4
samuel <samiehave9@gmail.com>
2024-08-31 14:20:39 +0300

    Challenge 2

--- sample.rb
+++ sample.rb
@@ -33,5 +33,27 @@ substrings("marcedesbenz", "benzmarcedes", "marcedesbenz", dictionary3)
 # Why does it return 'marcedesbenz' in the result?
 { "marcedes" => 2, "benz" => 2, "marcedesbenz" => 2, "benzmarcedes" => 2 }
 
-# Changing a string into an array of substrings
-# At this point we 
+dictionary-4 = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
+
+def substrings4(str, dictionary-4)
+  str.downcase
+  str_arr = str.split
+  match_arr = []
+  count_arr = []
+
+  dictionary-4.each do |i|
+    # if str_arr.any? { |s| s.iclude?(i) }
+    #  match_arr.push(i)
+    str_arr.each do |j|
+      # Code below checks if the substring is included in the string
+      if j.include?(i)
+        # Code below pushes the substring into the match_arr
+        match_arr.push(i)
+      end
+      count = str_arr.count { |s| s.include?(i) }
+      # Code below pushes the count of the substring into the count_arr
+      count_arr.push(count)
+      # Code below removes zeros from the count_arr
+      count_arr.delete(0)
+    end
+  end
